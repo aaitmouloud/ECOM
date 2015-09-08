00fd9574ff65b3b6e6479af255a8c4bf7bf26659
aitmoula <a.aitmouloud@outlook.com>
2015-09-08 16:41:26 +0200

    Config log4j

--- EComGames/EComGames-ejb/src/main/java/fr/imag/ejb/Initializer.java
+++ EComGames/EComGames-ejb/src/main/java/fr/imag/ejb/Initializer.java
@@ -19,22 +19,26 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.apache.log4j.Logger;
 
 /**
  *
- * @author min
+ * @author aaitmouloud
  */
 @Singleton
 @LocalBean
 @Startup
 public class Initializer {
 
+    final static private Logger LOGGER = Logger.getLogger(Initializer.class);
+
     @PersistenceContext(unitName = "EComGamesPU")
     private EntityManager em;
 
     @PostConstruct
     void init() {
-        Editeur editeur = new Editeur("Sega Games", "C'est plus fort que toi", null);
+        LOGGER.info("Initialisation des données.");
+        Editeur editeur = new Editeur("Sega Games",LOGGER.getAllAppenders()+" C'est plus fort que toi", null);
         Jeu jeu = new Jeu("Legend of Zelda", "Link sauve Zelda", 1990, 3, "ftp://");
         Cle cle = new Cle(jeu);
         jeu.setEditeur(editeur);
@@ -42,19 +46,20 @@ public class Initializer {
         PrixJeu prixJeu = new PrixJeu(jeu, Calendar.getInstance(), null, 25D);
         PrixJeu prixJeu2 = new PrixJeu(jeu, Calendar.getInstance(), null, 77D);
         em.persist(jeu);
-        
-        Utilisateur user = new Utilisateur("testUser", "monMdp", Calendar.getInstance(), "bla@bla.bel");
+
+        Utilisateur user = new Utilisateur("unU", "monMdp", Calendar.getInstance(), "bla@bla.bel");
         em.persist(user);
-        
+
         user.setEmail("blabla@car.com");
-        em.merge(user);    
-        
+        em.merge(user);
+
         Achat achat = new Achat(user, Calendar.getInstance(), cle);
         em.persist(achat);
-        
+
         Jeu jeu2 = new Jeu("Mario Bros", "Mario sauve Peach", 1980, 3, "ftp://");
         jeu2.setEditeur(new Editeur("Nintendo", "Forever and ever after", null));
         jeu2.addCategorie(new Categorie("Jeu de plateforme"));
-        
+        em.persist(jeu2);
+        LOGGER.info("Fin de l'initialisation des données.");
     }
 }
