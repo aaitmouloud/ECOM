2cc64d951fecda48185f1dbcee30c0525c12ae57
aitmoula <a.aitmouloud@outlook.com>
2015-09-09 13:51:28 +0200

    Ajout des licences en entête des classes

--- EComGames/EComGames-ejb/src/main/java/fr/imag/dao/remote/IntRemotePrixJeuDAO.java
+++ EComGames/EComGames-ejb/src/main/java/fr/imag/dao/remote/IntRemotePrixJeuDAO.java
@@ -1,11 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Cette œuvre est mise à disposition selon les termes de la Licence Creative Commons Attribution.
+ * - Pas d’Utilisation Commerciale. 
+ * - Partage dans les Mêmes Conditions 4.0 International.
  */
 package fr.imag.dao.remote;
 
-import fr.imag.entities.PrixJeu;
 import fr.imag.entities.dto.JeuDTO;
 import fr.imag.entities.dto.PrixJeuDTO;
 import java.util.Collection;
